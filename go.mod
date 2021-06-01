41e5ebdddab211a36e20ef15c74de7e01a75b343
Bogdan Floris <bogdan.floris@gmail.com>
2021-06-01 23:11:27 +0300

    feature: jwt methods

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module bogdanfloris-com
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofiber/fiber/v2 v2.10.0
 	github.com/joho/godotenv v1.3.0
-	github.com/mattn/go-colorable v0.1.8
 	go.uber.org/zap v1.17.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 )
---
+++ internal/common/jwt/jwt.go
@@ -0,0 +1,28 @@
+package jwt
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+)
+
+func GenerateToken(claims jwt.Claims, secretKey string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func VerifyToken(token string, secretKey string) (*jwt.Token, jwt.MapClaims, error) {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	return parsedToken, parsedToken.Claims.(jwt.MapClaims), nil
+}
